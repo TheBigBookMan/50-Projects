c8d2ba344be09eba8a435f8620ad3dfda0abbb9a
Ben Smerd <bsmerd@adam.com.au>
2022-08-05 11:43:17 +0930

    init commit

---
+++ Project1/script.js
@@ -0,0 +1,14 @@
+var panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.toggle('active')
+    })
+})
+
+const removeActiveClasses = () => {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
