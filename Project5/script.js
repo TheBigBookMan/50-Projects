7722afb673c5913c9c5abfe0cc981c008af4d730
Ben Smerd <bsmerd@adam.com.au>
2022-08-15 11:54:37 +0930

    fixed bug project5

--- Project5/script.js
+++ Project5/script.js
@@ -17,6 +17,6 @@ function blurring() {
     bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
-const scale = (num, in_min, in_max, out-MIDIInput, out_max) => {
+const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
