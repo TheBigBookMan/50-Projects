b98db55adc70fc8352d5580a31aff29d01664df8
Ben Smerd <bsmerd@adam.com.au>
2022-09-28 11:33:21 +0930

    bug fix

--- 3DBoxesBG/script.js
+++ 3DBoxesBG/script.js
@@ -1,7 +1,7 @@
 const boxesContainer = document.getElementById('boxes');
 const btn = document.getElementById('btn');
 
-btn.addEventListener('click', () => boxesContainer.classList.toggle('.big'))
+btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
 
 function createBoxes() {
     for(let i = 0; i < 4; i++) {
