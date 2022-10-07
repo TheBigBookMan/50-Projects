3a9b892cc5540da7b6a1f80504945008e3a1167e
Ben Smerd <bsmerd@adam.com.au>
2022-10-07 14:42:02 +1030

    testimonials js

---
+++ Testimonials/script.js
@@ -0,0 +1,13 @@
+const testimonialsContainer = document.querySelector('.testimonials-container')
+const testimonial = document.querySelector('.testimonials')
+const userImage = document.querySelector('.user-image')
+const username = document.querySelector('.username')
+const role = document.querySelector('.role')
+
+// const testimonials = 
+
+let idx = 1
+
+function updateTestimonial() {
+    testimonials
+}
